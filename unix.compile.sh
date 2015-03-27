9e02f95a79d60e04fd35a4f24a8c1d0e017263a1
Thô <4mleth@gmail.com>
2015-03-27 19:22:42 +0100

    New specs

--- unix.compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-context --jit WP5_SPECIFICATIONS --result=WP5_SPECIFICATIONS --purgeall
--- win_compile.bat
+++ win_compile.bat
@@ -1,3 +1,4 @@
 dot -Tpng figs/graph_tag.dot -o figs/graph_tag.png
 context --jit WP5_SPECIFICATIONS --result=WP5_SPECIFICATIONS --purgeall
 REM context --jit WP5_HERBONAUTES --result=WP5_HERBONAUTES --purgeall
+REM context --jit component_collaboratoire/component_collaboratoire --result=WP5_COLLABORATOIRE --purgeall
